716a53865194a29a28a9135c3dfa1008784b9e24
Ayesha Siddiqua <128472306+Ayesha-Siddiqua88@users.noreply.github.com>
2023-07-03 18:54:49 +0530

    socks.cpp

---
+++ socks.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+  
+int main()
+  {
+    int T;
+    while(T>0){
+      int n;
+      cin>>n;
+      cout<<n+1<<endl;
+      
+      T++;
+    }
+    return 0;
+ }
