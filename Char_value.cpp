2e153abc034af45168283fdfb2a8ef6b73d41230
Ayesha Siddiqua <128472306+Ayesha-Siddiqua88@users.noreply.github.com>
2023-07-05 13:31:07 +0530

    Char_value.cpp

---
+++ Char_value.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+ int main()
+  {
+    char n;
+    cin>>n;
+   
+    if (n=='P' || n=='p')
+    {
+      cout<<"PrepBytes"; 
+    }
+    
+    else if (n=='Z' || n=='z')
+    {
+      cout<<"Zenith";
+    }
+    
+    else if (n=='E' || n=='e')
+    {
+      cout<<"Expert Coder"; 
+    }
+    
+    else if(n=='D' || n=='d')
+    {
+      cout<<"Data Structure";
+    } 
+    
+  }
+  
